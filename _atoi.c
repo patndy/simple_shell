51fed3ee356573f6ac880181ba366f5b45bdc10d
Maibarewa <najibusmanalasan@gmail.com>
2022-11-17 03:20:13 -0800

    the branch

---
+++ _atoi.c
@@ -0,0 +1,71 @@
+#include "shell.h"
+
+/**
+ * interactive - returns true if shell is in intractive mode
+ * @info: struct address
+ *
+ * Return: 1 if intractive mode, 0 otherwise
+ */
+int interactive(info_t *info)
+{
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
+
+/**
+ * is_delim - checks if charater is a delimeter
+ * @c: The character to check
+ * @delim: the delimeter string
+ * Return: 1 if true, 0 if false
+ */
+int is_delim(char c, char *delim)
+{
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
+	return (0);
+}
+
+/**
+ * _isalpha - checks for alphabet charcter
+ * @c: the chracter to input
+ *
+ * Return: 1 if c is alphabetic, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'z'))
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ * _atoi - converts s strings to an integer
+ * @s: The string to br converted
+ * Return: 0 if no numbers in string, converted number otherwise
+ *
+ */
+int _atoi(char *s)
+{
+	int i, sign = 1, flag = 0, output;
+	unsigned int result = 0;
+
+	for (i = 0; s[i] != '\0' && flag != 2; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+	return (output);
+}
